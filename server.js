be03fafdbfdd4cadfb402d839d31ec0619e62488
MPx <hellomariap@gmail.com>
2021-07-22 21:39:51 -0700

    Added post WIP

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs = require("fs")
 const express = require("express");
 const path = require("path");
 const uuid = require("uuid/v4");
+const { error } = require("console");
 const app = express();
 const port = process.env.PORT || 3005;
 
@@ -18,11 +19,24 @@ app.get("/", (req, res) => {
 
 app.get("/notes", (req, res) => {
     return res.sendFile(path.join(__dirname, "public/notes.html"))
-})
+});
 
 app.get("/api/notes", (req, res) => {
     return res.json(notesJSON);
-})
+});
+
+
+app.post("/api/notes", function (req, res) {
+    const newEntry = req.body;
+    newEntry.id = uuid() ;
+    notesJSON.push(newEntry)
+    // console.log(notesJSON);
+
+    fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notesJSON), function (err) {
+    if (err){ error; } 
+    res.json(newEntry);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
